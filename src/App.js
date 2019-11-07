2147be155e5002981f7b795b70232577dd4c2ea0
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-07 16:23:41 -0500

    add heading comments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import {APOD_API} from "./data/api.nasa.gov/api";
 import account from "./data/api.nasa.gov/account";
 import "./App.css";
 
+/***************************************
+  STATES
+***************************************/
+
+
+/***************************************
+  COMPONENTS
+***************************************/
 function App () {
+
+
   return (
     <div className="App">
       {/*<p>Read through the instructions in the README.md file to build your NASA app! Have fun 🚀!</p>*/}
