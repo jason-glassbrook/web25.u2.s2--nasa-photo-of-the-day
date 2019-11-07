9bfbc973bbf9a541c4a0d9b443a903abebe9fde6
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-07 16:11:33 -0500

    create API class to handle (possibly) shared fns

--- src/data/api.nasa.gov/api.js
+++ src/data/api.nasa.gov/api.js
@@ -2,6 +2,37 @@
   API.NASA.GOV - APIs
 *******************************************************************************/
 
+class API {
+  constructor ({base_url , params}) {
+    this.base_url = base_url;
+    this.params = params;
+  }
+
+  query_params_url (params) {
+    // get _real_ params
+    const real_param_names = Object.keys (this["params"]);
+    const good_param_names = (Object.keys (this["params"])).filter (
+      (name) => (real_param_names.includes (name))
+    );
+    // create params url string
+    const params_url = (
+      good_param_names
+      .map ((name , i) => {
+        const first = ((i === 0) ? "?" : "");
+        const value = this.params[name].format (params[name]);
+        return (`${first}${name}=${value}`);
+      })
+      .join ("&")
+    );
+    //
+    return (params_url);
+  }
+
+  query_url (params) {
+    return (`${this.base_url}${this.query_params_url (params)}`);
+  }
+}
+
 export const APOD = {
   "base_url" : "https://api.nasa.gov/planetary/apod",
   "params" : {
