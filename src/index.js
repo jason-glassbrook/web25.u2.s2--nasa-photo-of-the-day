089138fe79c702f0f5e1b4d75c8a033788cbd921
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-07 18:25:00 -0500

    add packages: bootstrap, reactstrap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 /// styles ///
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 
 ReactDOM.render (<App /> , document.getElementById ("root"));
