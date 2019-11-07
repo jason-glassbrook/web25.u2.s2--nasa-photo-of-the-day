f4453882fa167bb09690a9193107bb8ea61125f2
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-07 16:28:00 -0500

    setup initial query state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import axios from "axios";
 import {APOD_API} from "./data/api.nasa.gov/api";
 import account from "./data/api.nasa.gov/account";
 import "./App.css";
+import { Date } from "core-js";
 
 /***************************************
   STATES
 ***************************************/
-
+const init = {
+  "query" : {
+    "api_key" : account.api_key,
+    "date" : Date.now (),
+    "hd" : true,
+  }
+}
 
 /***************************************
   COMPONENTS
 ***************************************/
 function App () {
-
-
+  const [query , setQuery] = React.useState (init.query);
+  //
   return (
     <div className="App">
       {/*<p>Read through the instructions in the README.md file to build your NASA app! Have fun 🚀!</p>*/}
