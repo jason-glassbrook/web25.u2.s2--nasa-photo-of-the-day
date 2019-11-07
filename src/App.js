7c01d56afdf5ddd640616f266f98fb10e8738ea2
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-07 16:17:46 -0500

    import api-related code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
+import axios from "axios";
+import {APOD_API} from "./data/api.nasa.gov/api";
+import account from "./data/api.nasa.gov/account";
 import "./App.css";
 
 function App () {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {/*<p>Read through the instructions in the README.md file to build your NASA app! Have fun 🚀!</p>*/}
     </div>
   );
 }
