8923b44b3ce9338f8e0139e1d075a577c507c5b9
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-07 15:59:45 -0500

    break out account and APOD api into own files

---
+++ src/data/api.nasa.gov/api.js
@@ -0,0 +1,56 @@
+/*******************************************************************************
+  API.NASA.GOV - APIs
+*******************************************************************************/
+
+export const APOD = {
+  "base_url" : "https://api.nasa.gov/planetary/apod",
+  "options" : {
+    "api_key" : {
+      "format" : (x) => (x),
+      "to" : function ({api_key}) {
+        return (`api_key=${this.format (api_key)}`);
+      },
+    },
+    "date" : {
+      "format" : (x) => {
+        const datetime = x.toISOString ();
+        const stop = datetime.indexOf ("T");
+        const date = datetime.slice (0 , stop);
+        return (date);
+      },
+      "to" : function ({date}) {
+        return (
+          `date=${this.format (date)}`
+        );
+      },
+    },
+    "hd" : {
+      "format" : (x) => (x ? "True" : "False"),
+      "to" : function ({hd}) {
+        return (`hd=${this.format (hd)}`);
+      },
+    },
+  },
+  "options_url" : function (options) {
+    // get _real_ options
+    const real_option_names = Object.keys (this["options"]);
+    const good_option_names = (Object.keys (this["options"])).filter (
+      (name) => (real_option_names.includes (name))
+    );
+    // create options url string
+    const options_url = (
+      good_option_names
+      .map ((name , i) => {
+        const first = ((i === 0) ? "?" : "");
+        const value = this["options"][name].format (options[name]);
+        return (`${first}${name}=${value}`);
+      })
+      .join ("&")
+    );
+    //
+    return (options_url);
+  },
+  "url" : function (options) {
+    return (`${this["base_url"]}${this["options_url"] (options)}`);
+  },
+};
