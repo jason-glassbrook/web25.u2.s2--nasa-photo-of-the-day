0213e4852d09e25bd143e5b17801128db05716b8
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-07 19:50:23 -0500

    create basic button component

---
+++ src/components/control/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Button (props) {
+  return (
+    <button className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
